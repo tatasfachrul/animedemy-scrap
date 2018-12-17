c9b81512b13b44c9de4b540b8c1c92b019e38670
trinanda <zidanecr7kaka@gmail.com>
2018-12-17 14:35:57 +0700

    got data from anime list and revision error handling code

--- latest_episodes.py
+++ latest_episodes.py
@@ -1,25 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-
-url = 'http://animeindo.video/anime-list-animeindo'
+url = 'http://animeindo.video/anime-list-animeindo/'
 page = requests.get(url, timeout=10)
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
 mid = soup.find_all('div', {'class': 'amin_box_mid_link'})
-del mid[0]
 
 def get_anime_list():
     try:
+        del mid[0]
         for i in mid:
             anime_list = {
                 'title': i.text,
                 'link:': i
             }
             print(anime_list)
-    except:
-        print('Exception')
+    except Exception as e:
+        print('Exception', e)
 
 get_anime_list()
 
