55ebae8e7278e25e3a3d7088a7b1393fb8802297
trinanda <zidanecr7kaka@gmail.com>
2018-12-17 12:36:42 +0700

    .

--- latest_episodes.py
+++ latest_episodes.py
@@ -10,7 +10,14 @@ soup = BeautifulSoup(page.text, 'html.parser')
 # left_column = soup.find('div', {'id': 'episodes'})
 
 
-li = soup.find('div', {'id': 'episodes'})
-children = li.find_all('div', {'class': 'episode'})
-for child in children:
-    print(child)
+episodes = soup.find('div', {'id': 'episodes'})
+episode = episodes.find_all('div', {'class': 'episode'}, recursive=False)
+
+for i in episode:
+    # print(('=====' * 35), '\n', i)
+    for img_and_a in i:
+        a = img_and_a.find('a')
+        images = img_and_a.find('img')
+        a_href = img_and_a.find('a href')
+        print(('=====' * 35), '\n', a)
+        # print(('=====' * 35), '\n', a_href)
