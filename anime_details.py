d55fc1ccfdce822df24c6e91b4f2cb2007e0a061
trinanda <zidanecr7kaka@gmail.com>
2018-12-20 10:43:21 +0700

    mencoba untuk membuat object dari data yang telah di fetching
    Took 1 hour 6 minutes

--- anime_details.py
+++ anime_details.py
@@ -1,52 +1,76 @@
 import re
-
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
+
+
 start_time = datetime.now()
 
+TITLE = None
+URL_VIDEO = None
+IMAGES = None
+DESCRIPTIONS = None
+
+
+object_videos_url = {}
 
 def anime_descriptions(url_anime_details):
     anime_description_page = BeautifulSoup(url_anime_details.text, 'html.parser')
     title = anime_description_page.find('div', {'class': 'amin_week_box_up1'})
     title = title.text
 
+    TITLE = title
+
     # episode_list = anime_description_page.find('div', {'class': 'episode_list'})
-
-    image = anime_description_page.find('div', {'class': 'cat_image'})
-
-    soup = BeautifulSoup(url_anime_details.text, 'html.parser')
-
-    descriptions = soup.find('h3', text='Description:').find_next_sibling()
-
-    pattern = re.compile(r'\w+:\s.*?')
-
-    data = dict(field.split(':') for field in descriptions.find_all(text=pattern))
-    # data['Synopsis'] = descriptions.find('p', text="Synopsis:").find_next_sibling("p").get_text()
-
-    # print('Time execution: ', datetime.now() - start_time)
 
     episode_list_parent = anime_description_page.find('div', {'class': 'desc_box_mid'})
     episode_list_children = episode_list_parent.find_all('div', {'class': 'episode_list'})
 
-
     for i in episode_list_children:
-        link_to_video =  i.a.get('href')
-
+        link_to_video = i.a.get('href')
 
         url_videos = requests.get(link_to_video)
-        # print(url_videos)
+
         video_link = BeautifulSoup(url_videos.text, 'html.parser')
 
         video_on_iframe = video_link.find('iframe', allow='encrypted-media' == False)
-        # a = video_on_iframe.find('iframe')
-
-        facebook_frame = '<iframe allow="encrypted-media" allowtransparency="true" frameborder="0" height="80" scrolling="no" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAnimeindoFans%2F&amp;tabs&amp;width=280&amp;height=180&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=true&amp;show_facepile=false&amp;appId=123434497681677" style="border:none;overflow:hidden" width="280"></iframe>'
 
         if video_on_iframe is not None:
-            print(video_on_iframe['src'])
+            URL_VIDEO = video_on_iframe['src']
+
+            object_videos_url = {
+                    'URL_VIDEO': URL_VIDEO
+            }
+
+            print(object_videos_url)
 
 
+    image = anime_description_page.find('div', {'class': 'cat_image'})
+    images = image.find_all('img')
 
+    for i in images:
+        IMAGES = i['src']
 
+    soup = BeautifulSoup(url_anime_details.text, 'html.parser')
+
+    # first locate the container with the desired fields
+    description = soup.find("h3", text="Description:").find_next_sibling()
+
+    # get all the ":"-separated fields into a dictionary
+    pattern = re.compile(r"\w+:\s.*?")
+
+    data_descriptions = dict(field.split(":") for field in description.find_all(text=pattern))
+    # data_descriptions["Synopsis"] = description.find("p", text="Synopsis:").find_next_sibling("p").get_text()
+    # print('Time execution: ', datetime.now() - start_time)
+
+    DESCRIPTIONS = data_descriptions
+
+    data_json = {
+        'TITLE': TITLE,
+        'URL_VIDEO': URL_VIDEO,
+        'IMAGES': IMAGES,
+        'DESCRIPTIONS': DESCRIPTIONS
+    }
+
+    # print(data_json)
 
--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+# from flask import Flask, jsonify
+#
 from latest_episodes import get_anime_list
-from anime_details import anime_descriptions
+#
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def index():
+#     return jsonify(Data=get_anime_list())
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=3000, debug=True)
+#
 
 get_anime_list()
